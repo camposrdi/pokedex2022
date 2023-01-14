c6dfebd351104558ba31e5ddc29dcdb8ea8277ed
Develop <camposr.diiana@gmail.com>
2023-01-14 11:56:43 -0600

    Se actualizan estilos y de agrega para ver información de Pokémon.

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Logout from './Logout';
 import Profile from './Profile';
+import './Principal.css';
+import Pokemon from './Pokemon';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
+const MySwal = withReactContent(Swal)
+const url = "https://pokeapi.co/api/v2/pokemon/"
+
 //import SearchBar from './SearchBar'
 
 
 
 function Principal(props) {
     let {children} = props;
+    const [name, setName] = useState('')
 
   return (
     <>
@@ -15,6 +24,7 @@ function Principal(props) {
    
 <nav className="navbar navbar-inverse">
    <h1 className="tituloprincipal"> Pokédex </h1>   
+   <img/>
   <div className="container-fluid">
     <div className="navbar-header">
      {/* <SearchBar url={url}>
@@ -24,10 +34,25 @@ function Principal(props) {
   
     <form className="navbar-form navbar">
       <div className="form-group">
-        <input type="text" className="form-control" placeholder="Enter name or id" name="search" />
+        <input type="text" className="form-control" placeholder="Enter name or id" name="search" onChange={(e)=>{
+          setName(e.target.value)
+        }} />
       </div>
       
-      <button type="button" className="btn btn-default" name="searchbt">Search</button>
+      <button type="button" className="btn btn-default" name="searchbt" onClick={()=>{
+         MySwal.fire({showClass: {
+                      popup: 'animate__animated animate__fadeInRight'
+                    },
+                    hideClass: {
+                      popup: 'animate__animated animate__fadeOutUp'
+                    }
+                  ,html:<div className="container">
+                  <Pokemon urlPokemon={url+name}>
+
+                  </Pokemon>
+                  </div>},
+              )
+      }}>Search</button>
     </form>
     
      <ul className="nav navbar-nav navbar-right">
