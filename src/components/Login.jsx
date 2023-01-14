ec409864bfbcb2d16b7b6bb750956e2a55295309
Develop <camposr.diiana@gmail.com>
2023-01-14 11:53:00 -0600

    Inicialmente se agrega las funciones de Login y el botón.

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
+import './Login.css';
+
 
 const Login = () => {
     const { loginWithRedirect, isAuthenticated } = useAuth0();
@@ -8,8 +10,17 @@ const Login = () => {
 
     <>
    {
-  ! isAuthenticated && (<button onClick={() => loginWithRedirect()}>Log In</button>)
+  ! isAuthenticated && (
+    <>
+      <button className='login' onClick={() => loginWithRedirect()}>Log In</button>
+        <div className='container'>
+        <h1 className='logintitulo'> Welcome my Pokédex </h1>
+      </div>
+      </>
+        )
    }
+  
+
     </>
   )
 }
